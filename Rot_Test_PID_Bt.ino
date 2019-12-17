cb246b589541b0553a1b5f7d7469cf840ad2da46
leobrenes <51466051+leobrenes@users.noreply.github.com>
2019-12-16 20:06:52 -0600

    Update Rot_Test_PID_Bt.ino
    @rangerdawavs @nicole-0 
    This new branch utilizes the Gyroscope to find current angle. It has a starting angle of 0, which is why it has an offset to adjust that number to the starting angle. Communications with the raspberry have 3 modes. This allows for correction of the coefficients during testing. Instead of sending the desired angle and magnitude, the current and desired coordinates are sent and the desired angle and magnitude are calculated in the Arduino. There might be some issues related to the type of variables. I was able to compile it in the Arduino IDE but the Raspberry Pi code must be updated to match this before it can be tested in the actual prototype. Comments need to be updated.

--- Rot_Test_PID_Bt.ino
+++ Rot_Test_PID_Bt.ino
@@ -2,79 +2,96 @@
  Editado para recibir comandos por el Serial
  Luego re editado para recibir un angulo por BlueTooth, calcular la diferencia entre el calculo deciado y el actual y usar un PD control system para mantener un angulo deseado
 */
-
+#include <MPU6050_tockn.h>
+#include <Wire.h>
 #include <RobotKit.h>
+MPU6050 mpu6050(Wire);
+
 // robotkit es una biblioteca de donde sacamos los functions de forward, backward y rotations usando dos motores
 
 //declarando las variables que usaremos para leer el Serial
-int zeta;//esta variable guarda el angulo donde esta apuntando el vehiculo
+int zeta, zetaOffset;//esta variable guarda el angulo donde esta apuntando el vehiculo y el offset para el gyroscopio 
 int alpha;//angulo al cual se tiene que llegar
-int mag; //distancia al centro
+float mag; //distancia al centro
 String a;// esta variable guarda temporalmete lo que se recibe del serial
-String b;// esta variable guarda un substring
-String c;// esta variable guarda un substring
-String d;// esta variable guarda un substring
-int error_Rot;
-int previousError_Rot;
+String b,c,d,e,f;// estas variable guardan un substring
+int error_Rot, previousError_Rot;
 float errorDot_Rot;
 float control_Rot;
-int error_Pos;
-int previousError_Pos;
+int error_Pos, previousError_Pos;
 float errorDot_Pos;
 float control_Pos;
 float k_P_Rot=1;// El coefficient del proportional  del angulo
 float k_D_Rot =0;// el coefficient de la derivada del angulo
 float k_P_Pos=1;// El coefficient del proportional del magnitude del error en position
 float k_D_Pos =0;// el coefficient de la derivada del magnitud del error en position
+float x_Current,y_Current, x_Desired, y_Desired;
 int previousTime;
 int currentTime;// these two are needed to find the derivative of the error
 int errorDeadband_Rot=1;// the deadband makes sure that the system is not overcompensating for negligable error
 int errorDeadband_Pos=1;
-int Msg ;// this variable stores the message number so classify the first and second as coefficients
+int Msg =4;// this variable stores the message number so classify the first and second as coefficients
 
 
 void setup() {
   setMotors();  // turn motors on
   Serial.begin(9600);
-  Msg=0;
+  Wire.begin();
+  mpu6050.begin();
+  mpu6050.calcGyroOffsets(true);
 } 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  if (Serial.available()){ //waits to detect something 
-
-    if( Msg<4){ //first 4 messages saved as coefficients
-       if(Msg=0){
-        a = Serial.readString();
-        k_P_Rot = a.toFloat(); //the coefficient that determines how important the error in the rotation is
-        Msg=1;  
-        
-        }
-       if(Msg=1){
-        a = Serial.readString();
-        k_D_Rot = a.toFloat(); //the coefficient that determines how important the derivative in the rotation is
-        Msg=2; 
-        }
-       if(Msg=2){
-        a = Serial.readString();
-        k_P_Pos = a.toFloat(); //the coefficient that determines how important the error in the position is
-        Msg=3; 
-        }
-     if(Msg=3){
-        a = Serial.readString();
-        k_D_Pos = a.toFloat(); //the coefficient that determines how important the derivative in the position is
-        Msg=4;
-        }
-
-  }
-    if( Msg>=4){ //from here on, the other messages are taken as data. received as triples
+  if (Serial.available()){ //If something is detected.
     a = Serial.readString();
-    b= getValue(a,',',0);
+    b= getValue(a,',',0);// this first bit number holds information about what the message is about. 
     c= getValue(a,',',1);
     d= getValue(a,',',2); //breaks down the triples into three strings
-    zeta = b.toInt();
-    alpha = c.toInt();
-    mag = d.toInt(); // changes string to integers
+    e= getValue(a,',',3);
+    f= getValue(a,',',4);
+   if(b==0){// in the case that it is just position information
+    x_Current= c.toFloat();
+    y_Current= d.toFloat();
+    x_Desired= e.toFloat();
+    y_Desired= f.toFloat();
+    if(y_Current<y_Desired){
+     alpha = atan((x_Current-x_Desired)/(y_Current-y_Desired));
+    }
+    if(y_Current>y_Desired){
+     if(y_Current=y_Desired){
+     if(x_Current>y_Desired){
+     alpha = 180-atan((x_Current-x_Desired)/(y_Current-y_Desired));
+     }
+     if(x_Current<y_Desired){
+     alpha = 180+-atan((x_Current-x_Desired)/(y_Current-y_Desired));
+     }
+    }
+    
+    if(y_Current=y_Desired){
+     if(x_Current>y_Desired){
+     alpha = -90;
+     }
+     if(x_Current<y_Desired){
+     alpha = 90;
+     }
+    }
+    
+    mag = sqrt(sq(x_Current-x_Desired)+sq(y_Current-y_Desired));
+   } 
+   if(b=1){// in the case that it is coefficient information
+      k_P_Rot=c.toFloat();// El coefficient del proportional  del angulo
+      k_D_Rot=d.toFloat();// el coefficient de la derivada del angulo
+      k_P_Pos=e.toFloat();// El coefficient del proportional del magnitude del error en position
+      k_D_Pos=f.toFloat();
+   } 
+   if(b==2){// in the case that it is the intinial angle setup information
+    zetaOffset = c.toInt();
+       }  
+  }
+    if( Msg>=4){ //this condition is always met. It is just there in case another condition needs to be added later
+    zeta = mpu6050.getAngleZ()+ zetaOffset;
+   
      Serial.print(zeta);
      Serial.print(',');
      Serial.print(alpha);
@@ -82,8 +99,9 @@ void loop() {
      Serial.println(mag); //prints data
   if((zeta <= 180 && zeta >= -180)){
    //fliters out unexpected values
-    currentTime= millis(); //records time since the last message was received
+   currentTime= millis(); //records time since the last message was received
    error_Rot= zeta-alpha; // calculates the error of the angle
+   error_Pos= mag-0; //calculates the error in the position
    if (error_Rot>180 or error_Rot<-180){ //interprets angles outside of the range in module 180 (taking in count sign)
     if (error_Rot>0){
      error_Rot = error_Rot-360;
@@ -91,12 +109,9 @@ void loop() {
     if (error_Rot<0){
      error_Rot = error_Rot+360;
     }
-    
    }
-     error_Pos= mag-0; //calculates the error in the position
     errorDot_Rot =(error_Rot-previousError_Rot)/(currentTime-previousTime); //calculates derivative
     errorDot_Pos =(error_Pos-previousError_Pos)/(currentTime-previousTime);
-    //Serial.println("detected ");
     previousError_Rot= error_Rot;
    previousError_Pos= error_Pos;
     previousTime=currentTime;    
@@ -105,17 +120,11 @@ void loop() {
     if(abs(control_Rot)>abs(errorDeadband_Rot) && -abs(errorDeadband_Rot)> -abs(control_Rot)){ //checks if the error is outside of the deadband 
     if (control_Rot<0){
       pivotLeft(100,abs(control_Rot));
-      //Serial.println("left");
     }
     if ( control_Rot> 0) {
       pivotRight(100,abs(control_Rot));
-      //Serial.println("right");
     }
-    
- 
-   
   stopMotors();
-   //delay(100);
     }
    if(abs(control_Rot)<abs(errorDeadband_Rot)&& -abs(errorDeadband_Rot)< -abs(control_Rot)){
     if(abs(control_Pos)>abs(errorDeadband_Pos) && -abs(errorDeadband_Pos)> -abs(control_Pos)){
@@ -125,18 +134,15 @@ void loop() {
     }
     if ( control_Pos> 0) {
       forward(100,abs(control_Pos));
-      //Serial.println("right");
     }
-    
   stopMotors();
-    
    }
   }
 }
   }
-}
-}
 
+}
+}
 String getValue(String data, char separator, int index)
 {
     int found = 0;
