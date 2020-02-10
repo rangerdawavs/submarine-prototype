30efaa7b1646bf97ccb16a02c9d80617e9b96378
leobrenes <51466051+leobrenes@users.noreply.github.com>
2020-02-10 00:49:04 -0600

    Update Rot_Test_PID_Bt.ino

--- Rot_Test_PID_Bt.ino
+++ Rot_Test_PID_Bt.ino
@@ -4,17 +4,19 @@
 */
 #include <MPU6050_tockn.h>
 #include <Wire.h>
+#include <math.h>
 #include <RobotKit.h>
 MPU6050 mpu6050(Wire);
-
+#include <BasicLinearAlgebra.h>
+using namespace BLA;
 // robotkit es una biblioteca de donde sacamos los functions de forward, backward y rotations usando dos motores
 
 //declarando las variables que usaremos para leer el Serial
-int zeta, zetaOffset;//esta variable guarda el angulo donde esta apuntando el vehiculo y el offset para el gyroscopio 
-int alpha;//angulo al cual se tiene que llegar
+int zeta,zetaOffset;//esta variable guarda el angulo donde esta apuntando el vehiculo y el offset para el gyroscopio 
+int alpha;
 float mag; //distancia al centro
 String a;// esta variable guarda temporalmete lo que se recibe del serial
-String b,c,d,e,f;// estas variable guardan un substring
+String b,c,d,e,f,g,h,i;// estas variable guardan un substring
 int error_Rot, previousError_Rot;
 float errorDot_Rot;
 float control_Rot;
@@ -30,20 +32,20 @@ int previousTime;
 int currentTime;// these two are needed to find the derivative of the error
 int errorDeadband_Rot=1;// the deadband makes sure that the system is not overcompensating for negligable error
 int errorDeadband_Pos=1;
-int Msg =4;// this variable stores the message number so classify the first and second as coefficients
+int Msg =0;// this variable stores the message number so classify the first and second as coefficients
 BLA::Matrix<4,4> Pos_StateTransition;
-BLA::Matrix<4,4> Pos_ControlMatrix;
+BLA::Matrix<4,2> Pos_ControlMatrix;
 BLA::Matrix<2> Pos_ControlVector;// the accelerometer output
-BLA::Matrix<2,2> Pos_PredictionVariance;// should be a identity matrix multiplied by the accelerometer variance
+BLA::Matrix<4,4> Pos_PredictionVariance;// should be a identity matrix multiplied by the accelerometer variance
 BLA::Matrix<4,4> Pos_MeasurmentVariance;//should be the camera variance diagonally and 0s in the remaining spaces
 BLA::Matrix<4> Pos_MeasurementVector;
 BLA::Matrix<4,4> Pos_StateCovarianceMatrix;
 BLA::Matrix<4,4> Pos_H;
 BLA::Matrix<4> PositionVelocityVector;
 BLA::Matrix<4,4> Pos_KalmanGain; 
-BLA::Matrix<4,4> Pos_identity = {1,0,0,0
-                                 0,1,0,0
-                                 0,0,1,0
+BLA::Matrix<4,4> Pos_identity = {1,0,0,0,
+                                 0,1,0,0,
+                                 0,0,1,0,
                                  0,0,0,1};
 
 BLA::Matrix<2,2> Rot_StateTransition;
@@ -58,7 +60,7 @@ BLA::Matrix<2> RotationVelocityVector;
 BLA::Matrix<2,2> Rot_KalmanGain; 
 BLA::Matrix<2,2> Rot_identity = {1,0, 
                                  0,1};
-
+float delta_t;//  the time between measurements
 void setup() {
   setMotors();  // turn motors on
   Serial.begin(9600);
@@ -69,7 +71,7 @@ void setup() {
 void loop() {
   
 
-  if (Serial.available()){ //If something is detected.
+  if (Serial.available()){ //If something is recieved.
     a = Serial.readString();
     b= getValue(a,',',0);// this first bit number holds information about what the message is about. 
     c= getValue(a,',',1);
@@ -79,12 +81,21 @@ void loop() {
     h= getValue(a,',',5);
     i= getValue(a,',',6);
    if(b==0){// in the case that it is information
-    Pi_x= c.toFloat();
-    Pi_x_Dot= d.toFloat();
-    Pi_y= e.toFloat();
-    Pi_y_Dot= f.toFloat();
+    PositionVelocityVector(1)= c.toFloat();
+    PositionVelocityVector(2)= d.toFloat();
+    PositionVelocityVector(3)= e.toFloat();
+    PositionVelocityVector(4)= f.toFloat();
     Rot_MeasurementVector(1)= g.toFloat();
     Rot_MeasurementVector(2)= h.toFloat();
+    //The following section calculates the Kalman Gain, the Vector and the covariance matrix for position and rotation
+    Pos_KalmanGain+=((Pos_StateCovarianceMatrix)*(Pos_StateCovarianceMatrix+Pos_MeasurmentVariance).Inverse())-Pos_KalmanGain ;
+    PositionVelocityVector+=Pos_KalmanGain*(Pos_MeasurementVector-PositionVelocityVector);
+    Pos_StateCovarianceMatrix += ((Pos_identity-Pos_KalmanGain)*Pos_StateCovarianceMatrix) - Pos_StateCovarianceMatrix;
+ 
+    Rot_KalmanGain+=((Rot_StateCovarianceMatrix)*(Rot_StateCovarianceMatrix+Rot_MeasurmentVariance).Inverse())-Rot_KalmanGain ;
+    RotationVelocityVector+=Rot_KalmanGain*(Rot_MeasurementVector-RotationVelocityVector);
+    Rot_StateCovarianceMatrix += ((Rot_identity-Rot_KalmanGain)*Rot_StateCovarianceMatrix) - Rot_StateCovarianceMatrix;
+    
     /* this section was commented since it calculates the desired angle using the current position. 
     if(y_Current<y_Desired){
      alpha = atan((x_Current-x_Desired)/(y_Current-y_Desired));
@@ -108,7 +119,7 @@ void loop() {
      }
     }
     */
-    mag = sqrt(sq(x_Current-x_Desired)+sq(y_Current-y_Desired));
+   
    } 
    if(b=1){// in the case that it is coefficient information
       k_P_Rot=c.toFloat();// El coefficient del proportional  del angulo
@@ -118,24 +129,60 @@ void loop() {
    } 
    if(b==2){// in the case that it is setup information
     zetaOffset = c.toInt();// this must be changed before testing
+    Msg=1;
        }  
   }
-    if( Msg>=4){ //this condition is always met. It is just there in case another condition needs to be added later
-    zeta = mpu6050.getAngleZ()+ zetaOffset;
+ 
+    if( Msg>=1){ //this condition is always met. It is just there in case another condition needs to be added later
+     delta_t= currentTime-previousTime;
+    zeta = 1;
+     //the followin section does the predict step of the Kalman filter
+     
+ Pos_StateTransition << 1, delta_t,  0,  0,
+                        0,    1   ,  0,  0,
+                        0,    0   ,  1,delta_t,
+                        0,    0   ,  0,  1;
+ Pos_ControlMatrix << sq(delta_t)/2,   0,
+                          0,         delta_t,
+                     sq(delta_t)/2,    0,
+                          0       , delta_t; 
+ Pos_ControlVector << ((mpu6050.getAccX())*sin(RotationVelocityVector(1)*PI/180)+(mpu6050.getAccY())*cos(RotationVelocityVector(1)*PI/180)),
+                      ((mpu6050.getAccX())*cos(RotationVelocityVector(1)*PI/180)+ (mpu6050.getAccY())*sin(-RotationVelocityVector(1)*PI/180));
+ Rot_StateTransition << 1, 0,
+                        0, 0;
+ Rot_ControlMatrix << delta_t,
+                        1; 
+ Rot_ControlVector<< (mpu6050.getAngleZ()+ zetaOffset) /delta_t;
    
-     Serial.print(zeta);
-     Serial.print(',');
-     Serial.print(alpha);
-     Serial.print(',');
+  PositionVelocityVector+=((Pos_StateTransition*PositionVelocityVector)+(Pos_ControlMatrix*Pos_ControlVector))-PositionVelocityVector;
+  Pos_StateCovarianceMatrix +=(Pos_StateTransition* Pos_StateCovarianceMatrix*~Pos_StateTransition)+(Pos_ControlMatrix*~Pos_ControlMatrix*Pos_PredictionVariance)-Pos_StateCovarianceMatrix;
+  RotationVelocityVector+=((Rot_StateTransition*RotationVelocityVector)+(Rot_ControlMatrix*Rot_ControlVector))-RotationVelocityVector;
+  Rot_StateCovarianceMatrix +=(Rot_StateTransition* Rot_StateCovarianceMatrix*~Rot_StateTransition)+(Rot_ControlMatrix*~Rot_ControlMatrix*Rot_PredictionVariance)-Rot_StateCovarianceMatrix;
+ // the following section will correct the Rot_Velocity vector to make sure its output is always between -180 and 180 just like the output of the raspbeery
+
+    zeta= (int)PositionVelocityVector(1);
+      if(abs(zeta)%360>180) {
+      zeta=(-180+((abs(zeta)%180)))*abs((zeta)/zeta);
+      }
+      if(abs(zeta)%360<180){
+        zeta=1*(zeta%180);
+      }
+      if(abs(zeta%360==180)){
+        zeta=180;
+      }  
+    PositionVelocityVector(1)=zeta;
      
-     Serial.println(mag); //prints data
      
+     
+     
+     mag = sqrt(sq(x_Current-x_Desired)+sq(y_Current-y_Desired));
      // this section starts the control loop
+     currentTime= millis();
   if((zeta <= 180 && zeta >= -180)){
    //fliters out unexpected values
-   currentTime= millis(); //records time since the last message was received
-   error_Rot= zeta-alpha; // calculates the error of the angle
-   error_Pos= mag-0; //calculates the error in the position
+   
+   //error_Rot= zeta-alpha; // calculates the error of the angle
+   //error_Pos= mag-0; //calculates the error in the position
    if (error_Rot>180 or error_Rot<-180){ //interprets angles outside of the range in module 180 (taking in count sign)
     if (error_Rot>0){
      error_Rot = error_Rot-360;
@@ -144,10 +191,10 @@ void loop() {
      error_Rot = error_Rot+360;
     }
    }
-    errorDot_Rot =(error_Rot-previousError_Rot)/(currentTime-previousTime); //calculates derivative
-    errorDot_Pos =(error_Pos-previousError_Pos)/(currentTime-previousTime);
-    previousError_Rot= error_Rot;
-   previousError_Pos= error_Pos;
+    //errorDot_Rot =(error_Rot-previousError_Rot)/(currentTime-previousTime); //calculates derivative
+   //errorDot_Pos =(error_Pos-previousError_Pos)/(currentTime-previousTime);
+    //previousError_Rot= error_Rot;
+   //previousError_Pos= error_Pos;
     previousTime=currentTime;    
     control_Rot = -k_P_Rot*error_Rot +k_D_Rot*errorDot_Rot; //creates a signal using the PID
    control_Pos = -k_P_Pos*error_Pos +k_D_Pos*errorDot_Pos;
@@ -174,9 +221,9 @@ void loop() {
   }
 }
   }
+}
 
-}
-}
+
 String getValue(String data, char separator, int index)
 {
     int found = 0;
